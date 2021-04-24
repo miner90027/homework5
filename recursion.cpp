7bbe25ffd44eac6b907e043e70da8536315b0032
miner90027 <alekszander.mccormick@outlook.com>
2021-04-24 12:49:07 -0800

    - wrote the iterative version of fib_loop() which returns the nth number of the fibonacci sequence using a loop

--- recursion.cpp
+++ recursion.cpp
@@ -11,16 +11,25 @@ int fib_loop(int n);
 
 
 int main() {
-	std::cout << fib(15) << std::endl;
+	std::cout << fib(5) << std::endl;
+	std::cout << fib_loop(5) << std::endl;
 	return 0;
 }
 
 int fib(int n){
-	if(n == 0) return n;
-	if(n == 1) return n;
+	if(n <= 1) return n;
 	return (fib(n-1) + fib(n-2));
 }
 
 int fib_loop(int n){
-	return n;
+	int sum = 0;
+	int n1 = 0;
+	int n2 = 1;
+
+	for(int i = 2; i <= n; i++){
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+	}
+	return sum;
 }
